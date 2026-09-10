59b1a807a59e749f8de776effeaefee7f6da9dd2
Medoune <diattamedounesambiane@gmail.com>
2026-09-10 20:29:48 +0200

    ajout de la logique de récuperation et l'affichage dynamique dans projetdetail

--- src/app/projet-detail/projet-detail.ts
+++ src/app/projet-detail/projet-detail.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Projet } from '../projet.model';
+import { LISTE_PROJETS } from '../projet.data';
 
 @Component({
-  imports: [],
   selector: 'app-projet-detail',
-  styleUrl: './projet-detail.css',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './projet-detail.html',
+  styleUrl: './projet-detail.css',
 })
-export class ProjetDetail {}
+export class ProjetDetail implements OnInit {
+  projetTrouve: Projet | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    // On récupère l'ID depuis l'URL
+    const idParam = this.route.snapshot.paramMap.get('id');
+    
+    if (idParam) {
+      const idNumber = Number(idParam);
+      //  On cherche le projet correspondant dans nos données statiques
+      this.projetTrouve = LISTE_PROJETS.find(p => p.id === idNumber);
+    }
+  }
+}
