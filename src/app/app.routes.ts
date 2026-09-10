0cfbd526f81b457bb74bc498d5bf76acc8fede9f
Medoune <diattamedounesambiane@gmail.com>
2026-09-10 20:09:54 +0200

    ajout de la logique de récuperation de l'id et de l'affichage dans le composant projet-detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+// 1. On importe le composant de détail pour qu'Angular le reconnaisse
+import { ProjetDetail } from './projet-detail/projet-detail';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // route par defaut pourla page d'accueil
+    {path: '', component:App},
+    
+  //  La route dynamique avec son ID
+  { path: 'projet/:id', component: ProjetDetail }
+];
