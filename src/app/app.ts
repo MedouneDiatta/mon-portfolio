08e37a01f5402ca1f3c6945a8523bb824c6bb3c1
Medoune <diattamedounesambiane@gmail.com>
2026-09-10 20:22:57 +0200

    mise en place de la navigation dynamique par id vers la page de detail

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,21 +1,19 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Pour les directives de base (optionnel mais recommandé)
+import { RouterOutlet, RouterLink } from '@angular/router'; // On ajoute RouterLink ici !
 
-// 1. On importe notre interface et notre liste de projets qu'on a créées avant
+// On importe notre interface et notre liste de projets
 import { Projet } from './projet.model';
 import { LISTE_PROJETS } from './projet.data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink], //  On les déclare tous les deux dans les imports
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  // Le titre de ton application (avec un signal)
   protected readonly title = signal('mon-portfolio');
-
-  // 2. On crée une propriété "listeProjets" qui contient tous tes projets.
-  // C'est cette variable que ton fichier HTML va lire pour afficher les cartes !
   listeProjets: Projet[] = LISTE_PROJETS;
 }
