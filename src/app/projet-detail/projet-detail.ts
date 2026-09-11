6db302c600a4d1a2d04ff2f439a7aa17684809bd
Medoune <diattamedounesambiane@gmail.com>
2026-09-11 20:47:54 +0200

    ajout de la logique de récuperation et du templete html pour le detail du projet

--- src/app/projet-detail/projet-detail.ts
+++ src/app/projet-detail/projet-detail.ts
@@ -1,29 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute, RouterLink } from '@angular/router';
-import { Projet } from '../projet.model';
-import { LISTE_PROJETS } from '../projet.data';
+import { RouterLink, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-projet-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './projet-detail.html',
   styleUrl: './projet-detail.css',
 })
 export class ProjetDetail implements OnInit {
-  projetTrouve: Projet | undefined;
+  // Variable qui contiendra le projet trouvé à afficher
+  projetTrouve: any;
 
+  // Liste temporaire des projets (la même que tu as sur l'accueil, à adapter si tu l'as mise dans un service)
+  listeProjets = [
+    { id: 1, titre: 'Application Web Angular', description: 'Création dun portfolio dynamique en binôme.', technos: ['Angular', 'TypeScript', 'Bootstrap'], lienGithub: 'https://github.com/MedouneDiatta/mon-portfolio' },
+    { id: 2, titre: 'Site E-commerce', description: 'Développement dun site de vente en ligne.', technos: ['HTML', 'CSS', 'JavaScript'], lienGithub: '' }
+  ];
+
+  // Injection de ActivatedRoute pour lire les paramètres de l'URL
   constructor(private route: ActivatedRoute) {}
 
-  ngOnInit(): void {
-    // On récupère l'ID depuis l'URL
+  ngOnInit() {
+    // 1. On récupère l'ID présent dans l'URL (ex: /projet/1 donne id = "1")
     const idParam = this.route.snapshot.paramMap.get('id');
     
     if (idParam) {
       const idNumber = Number(idParam);
-      //  On cherche le projet correspondant dans nos données statiques
-      this.projetTrouve = LISTE_PROJETS.find(p => p.id === idNumber);
+      // 2. On cherche dans notre tableau le projet qui correspond à cet ID
+      this.projetTrouve = this.listeProjets.find(p => p.id === idNumber);
     }
   }
 }
